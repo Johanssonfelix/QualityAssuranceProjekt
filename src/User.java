e82c45bbdf41dfea718340b857890603d6d3bb95
castiglioni <78161853+castiglioni@users.noreply.github.com>
2021-06-13 22:27:53 +0200

    Update User.java

--- src/User.java
+++ src/User.java
@@ -4,17 +4,16 @@ public class User{
     public String lastName;
     public int userType;
     public int maxLoans;
+    public int personalNumber;
 
 
-    public User() {
-    }
-
-    public User(int userId, String forname, String lastname, int userType, int maxloans) {
+    public User(int userId, String forname, String lastname, int userType, int maxloans, int personalNumber) {
         this.userId = userId;
         this.forName = forname;
         this.lastName = lastname;
         this.userType = userType;
         this.maxLoans = maxloans;
+        this.personalNumber = personalNumber;
     }
 
     public int getUserId() {
@@ -22,7 +21,12 @@ public class User{
     }
 
     public void setUserId(int userId) {
-        this.userId = userId;
+        if (userId.length() == 4){
+            this.userId = userId;
+        } else {
+            System.out.println("The userId must be 4 digits long.");
+        }
+        
     }
 
     public String getForName() {
@@ -52,24 +56,14 @@ public class User{
         return maxLoans;
     }
 
-    /*public void setUserType(int userType) {
+    public void setUserType(int userType) {
         if (userType == 0 || userType == 1 || userType == 2 || userType == 3 ){
             this.userType = userType;
         }else{
             System.out.println("Usertype must have an int from 0-3");
         }
-        if (userType == 0){
-            User student = new User();
-        } else if (userType == 1){
-            User phD = new User();
-        } else if (userType == 2){
-            User master = new User();
-        } else {
-            if (userType == 3){
-                User teacher = new User();
-            }
-        }
-    }*/
+        
+    }
 
     public void maxBooks(int userType){
         switch (userType){
@@ -79,5 +73,13 @@ public class User{
             case 4 -> this.setMaxLoans(10);
         }
 
+    }
+    
+    public void setPersonalNumber(int personalNumber) {
+        this.personalNumber = personalNumber;
+    }
+    
+    public int getPersonalNumber() {
+        return personalNumber;
     }
 }
