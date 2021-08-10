dfb5bf7bd4b119f461a659aa3d78283f3344b748
jakewaddup465 <85162166+jakewaddup465@users.noreply.github.com>
2021-08-10 17:46:37 +0200

    ReturnBook

--- src/LoanManager.java
+++ src/LoanManager.java
@@ -387,7 +387,7 @@ public class LoanManager extends Book {
         try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/Music?useSSL=false","root","Hanna0811");
             PreparedStatement nps = conn.prepareStatement("UPDATE book SET antal = antal + 1 WHERE isbn=?");//Inte säker på kommandot
-            PreparedStatement nnps = conn.prepareStatement("DELETE from bookuser where isbn = ? and loandId = ? ");//Inte säker på kommandot
+            PreparedStatement nnps = conn.prepareStatement("DELETE from bookuser where isbn = ? AND loandId = ? ");//Inte säker på kommandot
 
             try {
                 Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/Music?useSSL=false", "root", "Hanna0811");
